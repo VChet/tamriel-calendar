7e719ad1887cff5db486a719eb1b2bac23655434
VChet <17050347+VChet@users.noreply.github.com>
2023-05-11 21:58:44 +0400

    feat(PWA): add icons

--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,30 @@ const pwaOptions: Partial<VitePWAOptions> = {
   strategies: "injectManifest",
   srcDir: "src",
   filename: "sw.ts",
+  includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
   manifest: {
     name: "Tamriel Calendar",
     short_name: "Tamriel Calendar",
-    description: "Календарь с системой исчисления принятой во вселенной The Elder Scrolls"
+    description: "Календарь с системой исчисления принятой во вселенной The Elder Scrolls",
+    theme_color: "#fbf4e1",
+    icons: [
+      {
+        src: "icon-192x192.png",
+        sizes: "192x192",
+        type: "image/png"
+      },
+      {
+        src: "icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png"
+      },
+      {
+        src: "icon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable"
+      }
+    ]
   },
   workbox: { sourcemap: true }
 };
