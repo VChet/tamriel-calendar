54ca71b9b7968a58ce701dbf6adbc77a99d0055f
VChet <17050347+VChet@users.noreply.github.com>
2023-05-14 18:37:04 +0400

    chore: remove pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { createApp } from "vue";
 import { createI18n } from "vue-i18n";
-import { createPinia } from "pinia";
 import messages from "@/i18n/messages";
 import { setLocale, settings } from "@/store/settings";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/global.scss";
-
-const pinia = createPinia();
 
 export const i18n = createI18n({
   locale: "en",
@@ -18,4 +15,4 @@ export const i18n = createI18n({
 
 setLocale(settings.value.locale);
 
-createApp(App).use(pinia).use(i18n).use(router).mount("#app");
+createApp(App).use(i18n).use(router).mount("#app");
