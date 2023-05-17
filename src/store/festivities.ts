1363cd74710d4c127609b85bd8d487f905f80343
rudnovd <rudnov.d@gmail.com>
2023-05-17 23:16:38 +0500

    feat(festivities): dymamic load festivitie data

--- src/store/festivities.ts
+++ src/store/festivities.ts
@@ -1,26 +1,31 @@
 import { createGlobalState } from "@vueuse/core";
-import holidaysJson from "@/assets/holidays.json";
-import summoningDaysJson from "@/assets/summoningDays.json";
-import birthsignsJson from "@/assets/birthsigns.json";
+import { ref, unref } from "vue";
 import { Birthsign, Festivity } from "@/types/festivity";
+import { settings } from "@/store/settings";
 
 export const useFestivitiesStore = createGlobalState(() => {
-  const holidays: Map<string, Festivity> = new Map();
-  holidaysJson.forEach((day) => {
-    holidays.set(day.date, day);
-  });
-  const summoningDays: Map<string, Festivity> = new Map();
-  summoningDaysJson.forEach((day) => {
-    summoningDays.set(day.date, day);
-  });
-  const birthsigns: Map<string, Birthsign> = new Map();
-  birthsignsJson.forEach((sign) => {
-    birthsigns.set(sign.month, sign);
-  });
+  const holidays = ref<Map<string, Festivity>>(new Map());
+  const summoningDays = ref<Map<string, Festivity>>(new Map());
+  const birthsigns = ref<Map<string, Birthsign>>(new Map());
+
+  setFestivitiesData(settings.value.locale);
+
+  async function setFestivitiesData(locale: string) {
+    const holidaysPromise: Promise<{ default: Array<Festivity> }> = import(`@/assets/holidays.json`);
+    const summoningDaysPromise: Promise<{ default: Array<Festivity> }> = import(`@/assets/summoningDays.json`);
+    const birthsignsPromise: Promise<{ default: Array<Birthsign> }> = import(`@/assets/birthsigns/${locale}.json`);
+
+    const data = await Promise.all([holidaysPromise, summoningDaysPromise, birthsignsPromise]);
+    data[0].default.forEach((day) => holidays.value.set(day.date, day));
+    data[1].default.forEach((day) => summoningDays.value.set(day.date, day));
+    data[2].default.forEach((sign) => birthsigns.value.set(sign.month, sign));
+  }
 
   return {
-    holidays,
-    summoningDays,
-    birthsigns
+    holidays: unref(holidays),
+    summoningDays: unref(summoningDays),
+    birthsigns: unref(birthsigns),
+
+    setFestivitiesData
   };
 });
--- src/store/settings.ts
+++ src/store/settings.ts
@@ -4,6 +4,7 @@ import { useRegisterSW } from "virtual:pwa-register/vue";
 import { i18n } from "@/main";
 import tamrielEn from "@/assets/tamriel-en";
 import tamrielRu from "@/assets/tamriel-ru";
+import { useFestivitiesStore } from "./festivities";
 
 export const settings = useStorage("settings", { locale: "en" });
 
@@ -25,7 +26,10 @@ function getDayJSLocaleData(locale: string) {
   }
 }
 export function setLocale(locale: string) {
+  const { setFestivitiesData } = useFestivitiesStore();
+
   dayjs.locale(getDayJSLocaleData(locale));
   i18n.global.locale.value = locale;
   settings.value.locale = locale;
+  setFestivitiesData(locale);
 }
