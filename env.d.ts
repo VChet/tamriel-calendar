3b41d6b6133bdfe0ad92ee0649eb9ef91b2ebf86
VChet <17050347+VChet@users.noreply.github.com>
2024-07-17 03:43:54 +0500

    chore(env): remove *.vue declaration

--- env.d.ts
+++ env.d.ts
@@ -1,12 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module "*.vue" {
-  import type { ComponentOptions } from "vue";
-
-  const Component: ComponentOptions;
-  export default Component;
-}
-
 interface ImportMetaEnv {
   readonly VITE_GIT_COMMIT_HASH: string
   readonly VITE_GIT_COMMIT_DATE: string
